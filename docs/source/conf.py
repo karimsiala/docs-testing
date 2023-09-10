f9d51eaa2a943092bfd4d355aa624ff81544ff61
Karim Siala <61633482+karimsiala@users.noreply.github.com>
2023-09-10 22:32:55 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Open Roboverse Knowledge Base'
+copyright = '2023, Open Roboverse e.V.'
+author = 'Karim Siala'
 
 release = '0.1'
 version = '0.1.0'
